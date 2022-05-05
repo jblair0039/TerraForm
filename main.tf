964a62129d80d567da2bce0b0210ab538705cb8c
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2022-05-05 17:00:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,40 @@
+#Creates two vm instances in GCP quickly for testing 
 terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
+required_providers {
+google = {
+source = "hashicorp/google"
+}
+}
 }
 provider "google" {
-  version = "3.5.0"
-  project = "jblair0039-2019-v1"
-  region  = "us-east1"
-  zone    = "us-east1-b"
+version = "3.5.0"
+project = "jblair0039-2019-v1"
+region  = "us-east1"
+zone    = "us-east1-b"
 }
-resource "google_compute_instance" "vm_instance" {
-  name         = "tform1"
-  machine_type = "f1-micro"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-  resource "google_compute_instance" "vm_instance" {
-  name         = "tform2"
-  machine_type = "f1-micro"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+resource "google_compute_instance" "vm_instance1" {
+name = "tform1"
+machine_type = "f1-micro"
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-9"
+}
+}
 network_interface { 
-  network = "default" 
-  access_config {}
-} 
+network = "default" 
+access_config {}
+}
+}
+resource "google_compute_instance" "vm_instance2" {
+name = "tform2"
+machine_type = "f1-micro"
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-9"
+}
+}
 network_interface { 
-  network = "default" 
-  access_config {}
-} 
+network = "default" 
+access_config {}
+}
+}
