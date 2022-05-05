97f7e5cba7f2e5d875428f690df2269d341b9d4e
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2022-05-05 15:49:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "jblair0039-2019-v1"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "tform1"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+network_interface { 
+  network = "default" 
+  access_config {}
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "tform2"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+network_interface { 
+  network = "default" 
+  access_config {}
